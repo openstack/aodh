73bc82ba6041c17c10a55cd853a2ea5a5c1f97d1
liusheng <liusheng@huawei.com>
2015-12-23 15:26:25 +0800

    Add composite rule alarm evaluator
    Add a composite rule alarm evaluator, which will evaluate composite alarms
    based on threshold evaluators.
    
    blueprint composite-threshold-rule-alarm
    
    Change-Id: Ie0df81856425c5c1ad7f8620c7d153363c987fd3

---
+++ aodh/evaluator/composite.py
@@ -0,0 +1,236 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+from oslo_log import log
+import six
+from stevedore import NamedExtensionManager
+
+from aodh import evaluator
+from aodh.i18n import _
+
+LOG = log.getLogger(__name__)
+
+STATE_CHANGE = {evaluator.ALARM: 'outside their threshold.',
+                evaluator.OK: 'inside their threshold.',
+                evaluator.UNKNOWN: 'state evaluated to unknown.'}
+
+
+class RuleTarget(object):
+
+    def __init__(self, rule, rule_evaluator, rule_name):
+        self.rule = rule
+        self.type = rule.get('type')
+        self.rule_evaluator = rule_evaluator
+        self.rule_name = rule_name
+        self.state = None
+        self.trending_state = None
+        self.statistics = None
+        self.evaluated = False
+
+    def evaluate(self):
+        # Evaluate a sub-rule of composite rule
+        if not self.evaluated:
+            LOG.debug('Evaluating %(type)s rule: %(rule)s',
+                      {'type': self.type, 'rule': self.rule})
+            self.state, self.trending_state, self.statistics, __ = \
+                self.rule_evaluator.evaluate_rule(self.rule)
+            self.evaluated = True
+
+
+class RuleEvaluationBase(object):
+    def __init__(self, rule_target):
+        self.rule_target = rule_target
+
+    def __str__(self):
+        return self.rule_target.rule_name
+
+
+class OkEvaluation(RuleEvaluationBase):
+
+    def __bool__(self):
+        self.rule_target.evaluate()
+        return self.rule_target.state == evaluator.OK
+
+    __nonzero__ = __bool__
+
+
+class AlarmEvaluation(RuleEvaluationBase):
+
+    def __bool__(self):
+        self.rule_target.evaluate()
+        return self.rule_target.state == evaluator.ALARM
+
+    __nonzero__ = __bool__
+
+
+class AndOp(object):
+    def __init__(self, rule_targets):
+        self.rule_targets = rule_targets
+
+    def __bool__(self):
+        return all(self.rule_targets)
+
+    def __str__(self):
+        return '(' + ' and '.join(six.moves.map(str, self.rule_targets)) + ')'
+
+    __nonzero__ = __bool__
+
+
+class OrOp(object):
+    def __init__(self, rule_targets):
+        self.rule_targets = rule_targets
+
+    def __bool__(self):
+        return any(self.rule_targets)
+
+    def __str__(self):
+        return '(' + ' or '.join(six.moves.map(str, self.rule_targets)) + ')'
+
+    __nonzero__ = __bool__
+
+
+class CompositeEvaluator(evaluator.Evaluator):
+    def __init__(self, conf):
+        super(CompositeEvaluator, self).__init__(conf)
+        self.conf = conf
+        self._threshold_evaluators = None
+        self.rule_targets = []
+        self.rule_name_prefix = 'rule'
+        self.rule_num = 0
+
+    @property
+    def threshold_evaluators(self):
+        if not self._threshold_evaluators:
+            threshold_types = ('threshold', 'gnocchi_resources_threshold',
+                               'gnocchi_aggregation_by_metrics_threshold',
+                               'gnocchi_aggregation_by_resources_threshold')
+            self._threshold_evaluators = NamedExtensionManager(
+                'aodh.evaluator', threshold_types, invoke_on_load=True,
+                invoke_args=(self.conf,))
+        return self._threshold_evaluators
+
+    def _parse_composite_rule(self, alarm_rule):
+        """Parse the composite rule.
+
+        The composite rule is assembled by sub threshold rules with 'and',
+        'or', the form can be nested. e.g. the form of composite rule can be
+        like this:
+        {
+            "and": [threshold_rule0, threshold_rule1,
+                    {'or': [threshold_rule2, threshold_rule3,
+                            threshold_rule4, threshold_rule5]}]
+        }
+        """
+        if (isinstance(alarm_rule, dict) and len(alarm_rule) == 1
+                and list(alarm_rule)[0] in ('and', 'or')):
+            and_or_key = list(alarm_rule)[0]
+            if and_or_key == 'and':
+                rules = (self._parse_composite_rule(r) for r in
+                         alarm_rule['and'])
+                rules_alarm, rules_ok = zip(*rules)
+                return AndOp(rules_alarm), OrOp(rules_ok)
+            else:
+                rules = (self._parse_composite_rule(r) for r in
+                         alarm_rule['or'])
+                rules_alarm, rules_ok = zip(*rules)
+                return OrOp(rules_alarm), AndOp(rules_ok)
+        else:
+            rule_evaluator = self.threshold_evaluators[alarm_rule['type']].obj
+            self.rule_num += 1
+            name = self.rule_name_prefix + str(self.rule_num)
+            rule = RuleTarget(alarm_rule, rule_evaluator, name)
+            self.rule_targets.append(rule)
+            return AlarmEvaluation(rule), OkEvaluation(rule)
+
+    def _reason(self, alarm, new_state, rule_target_alarm):
+        transition = alarm.state != new_state
+        reason_data = {
+            'type': 'composite',
+            'composition_form': str(rule_target_alarm)}
+        root_cause_rules = {}
+        for rule in self.rule_targets:
+            if rule.state == new_state:
+                root_cause_rules.update({rule.rule_name: rule.rule})
+        reason_data.update(causative_rules=root_cause_rules)
+        params = {'state': new_state,
+                  'expression': str(rule_target_alarm),
+                  'rules': ', '.join(sorted(root_cause_rules)),
+                  'description': STATE_CHANGE[new_state]}
+        if transition:
+            reason = (_('Composite rule alarm with composition form: '
+                        '%(expression)s transition to %(state)s, due to '
+                        'rules: %(rules)s %(description)s') % params)
+
+        else:
+            reason = (_('Composite rule alarm with composition form: '
+                        '%(expression)s remaining as %(state)s, due to '
+                        'rules: %(rules)s %(description)s') % params)
+
+        return reason, reason_data
+
+    def _evaluate_sufficient(self, alarm, rule_target_alarm, rule_target_ok):
+        # Some of evaluated rules are unknown states or trending states.
+        unknown = alarm.state == evaluator.UNKNOWN
+        continuous = alarm.repeat_actions
+        if unknown or continuous:
+            for rule in self.rule_targets:
+                if rule.trending_state:
+                    rule.state = (rule.trending_state if unknown
+                                  else alarm.state)
+
+        alarm_triggered = bool(rule_target_alarm)
+        if alarm_triggered:
+            reason, reason_data = self._reason(alarm, evaluator.ALARM,
+                                               rule_target_alarm)
+            self._refresh(alarm, evaluator.ALARM, reason, reason_data)
+            return True
+
+        ok_result = bool(rule_target_ok)
+        if ok_result:
+            reason, reason_data = self._reason(alarm, evaluator.OK,
+                                               rule_target_alarm)
+            self._refresh(alarm, evaluator.OK, reason, reason_data)
+            return True
+        return False
+
+    def evaluate(self, alarm):
+        if not self.within_time_constraint(alarm):
+            LOG.debug('Attempted to evaluate alarm %s, but it is not '
+                      'within its time constraint.', alarm.alarm_id)
+            return
+
+        LOG.debug("Evaluating composite rule alarm %s ...", alarm.alarm_id)
+        self.rule_targets = []
+        self.rule_num = 0
+        rule_target_alarm, rule_target_ok = self._parse_composite_rule(
+            alarm.rule)
+
+        sufficient = self._evaluate_sufficient(alarm, rule_target_alarm,
+                                               rule_target_ok)
+        if not sufficient:
+            for rule in self.rule_targets:
+                rule.evaluate()
+            sufficient = self._evaluate_sufficient(alarm, rule_target_alarm,
+                                                   rule_target_ok)
+
+        if not sufficient:
+            # The following unknown situations is like these:
+            # 1. 'unknown' and 'alarm'
+            # 2. 'unknown' or 'ok'
+            reason, reason_data = self._reason(alarm, evaluator.UNKNOWN,
+                                               rule_target_alarm)
+            if alarm.state != evaluator.UNKNOWN:
+                self._refresh(alarm, evaluator.UNKNOWN, reason, reason_data)
+            else:
+                LOG.debug(reason)
---
+++ aodh/tests/unit/evaluator/test_composite.py
@@ -0,0 +1,407 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Tests for aodh/evaluator/composite.py
+"""
+
+import uuid
+
+from ceilometerclient.v2 import statistics
+import mock
+from oslo_utils import timeutils
+from oslotest import mockpatch
+import six
+from six import moves
+
+from aodh import evaluator
+from aodh.evaluator import composite
+from aodh.storage import models
+from aodh.tests import constants
+from aodh.tests.unit.evaluator import base
+
+
+class TestEvaluate(base.TestEvaluatorBase):
+    EVALUATOR = composite.CompositeEvaluator
+
+    sub_rule1 = {
+        "type": "threshold",
+        "meter_name": "cpu_util",
+        "evaluation_periods": 5,
+        "threshold": 0.8,
+        "query": [{
+            "field": "metadata.metering.stack_id",
+            "value": "36b20eb3-d749-4964-a7d2-a71147cd8147",
+            "op": "eq"
+        }],
+        "statistic": "avg",
+        "period": 60,
+        "exclude_outliers": False,
+        "comparison_operator": "gt"
+    }
+
+    sub_rule2 = {
+        "type": "threshold",
+        "meter_name": "disk.iops",
+        "evaluation_periods": 4,
+        "threshold": 200,
+        "query": [{
+            "field": "metadata.metering.stack_id",
+            "value": "36b20eb3-d749-4964-a7d2-a71147cd8147",
+            "op": "eq"
+        }],
+        "statistic": "max",
+        "period": 60,
+        "exclude_outliers": False,
+        "comparison_operator": "gt"
+    }
+
+    sub_rule3 = {
+        "type": "threshold",
+        "meter_name": "network.incoming.packets.rate",
+        "evaluation_periods": 3,
+        "threshold": 1000,
+        "query": [{
+            "field": "metadata.metering.stack_id",
+            "value": "36b20eb3-d749-4964-a7d2-a71147cd8147",
+            "op": "eq"
+        }],
+        "statistic": "avg",
+        "period": 60,
+        "exclude_outliers": False,
+        "comparison_operator": "gt"
+    }
+
+    sub_rule4 = {
+        "type": "gnocchi_resources_threshold",
+        'comparison_operator': 'gt',
+        'threshold': 80.0,
+        'evaluation_periods': 5,
+        'aggregation_method': 'mean',
+        'granularity': 60,
+        'metric': 'cpu_util',
+        'resource_type': 'instance',
+        'resource_id': 'my_instance',
+    }
+
+    sub_rule5 = {
+        "type": "gnocchi_aggregation_by_metrics_threshold",
+        'comparison_operator': 'le',
+        'threshold': 10.0,
+        'evaluation_periods': 4,
+        'aggregation_method': 'max',
+        'granularity': 300,
+        'metrics': ['0bb1604d-1193-4c0a-b4b8-74b170e35e83',
+                    '9ddc209f-42f8-41e1-b8f1-8804f59c4053']
+    }
+
+    sub_rule6 = {
+        "type": "gnocchi_aggregation_by_resources_threshold",
+        'comparison_operator': 'gt',
+        'threshold': 80.0,
+        'evaluation_periods': 6,
+        'aggregation_method': 'mean',
+        'granularity': 50,
+        'metric': 'cpu_util',
+        'resource_type': 'instance',
+        'query': '{"=": {"server_group": "my_autoscaling_group"}}'
+    }
+
+    def prepare_alarms(self):
+        self.alarms = [
+            models.Alarm(name='alarm_threshold_nest',
+                         description='alarm with sub rules nested combined',
+                         type='composite',
+                         enabled=True,
+                         user_id='fake_user',
+                         project_id='fake_project',
+                         alarm_id=str(uuid.uuid4()),
+                         state='insufficient data',
+                         state_timestamp=constants.MIN_DATETIME,
+                         timestamp=constants.MIN_DATETIME,
+                         insufficient_data_actions=[],
+                         ok_actions=[],
+                         alarm_actions=[],
+                         repeat_actions=False,
+                         time_constraints=[],
+                         rule={
+                             "or": [self.sub_rule1,
+                                    {"and": [self.sub_rule2, self.sub_rule3]
+                                     }]
+                         },
+                         severity='critical'),
+            models.Alarm(name='alarm_threshold_or',
+                         description='alarm on one of sub rules triggered',
+                         type='composite',
+                         enabled=True,
+                         user_id='fake_user',
+                         project_id='fake_project',
+                         state='insufficient data',
+                         state_timestamp=constants.MIN_DATETIME,
+                         timestamp=constants.MIN_DATETIME,
+                         insufficient_data_actions=[],
+                         ok_actions=[],
+                         alarm_actions=[],
+                         repeat_actions=False,
+                         alarm_id=str(uuid.uuid4()),
+                         time_constraints=[],
+                         rule={
+                             "or": [self.sub_rule1, self.sub_rule2,
+                                    self.sub_rule3]
+                         },
+                         severity='critical'
+                         ),
+            models.Alarm(name='alarm_threshold_and',
+                         description='alarm on all the sub rules triggered',
+                         type='composite',
+                         enabled=True,
+                         user_id='fake_user',
+                         project_id='fake_project',
+                         state='insufficient data',
+                         state_timestamp=constants.MIN_DATETIME,
+                         timestamp=constants.MIN_DATETIME,
+                         insufficient_data_actions=[],
+                         ok_actions=[],
+                         alarm_actions=[],
+                         repeat_actions=False,
+                         alarm_id=str(uuid.uuid4()),
+                         time_constraints=[],
+                         rule={
+                             "and": [self.sub_rule1, self.sub_rule2,
+                                     self.sub_rule3]
+                         },
+                         severity='critical'
+                         ),
+            models.Alarm(name='alarm_multi_type_rules',
+                         description='alarm with threshold and gnocchi rules',
+                         type='composite',
+                         enabled=True,
+                         user_id='fake_user',
+                         project_id='fake_project',
+                         alarm_id=str(uuid.uuid4()),
+                         state='insufficient data',
+                         state_timestamp=constants.MIN_DATETIME,
+                         timestamp=constants.MIN_DATETIME,
+                         insufficient_data_actions=[],
+                         ok_actions=[],
+                         alarm_actions=[],
+                         repeat_actions=False,
+                         time_constraints=[],
+                         rule={
+                             "and": [self.sub_rule2, self.sub_rule3,
+                                     {'or': [self.sub_rule1, self.sub_rule4,
+                                             self.sub_rule5, self.sub_rule6]}]
+                         },
+                         severity='critical'
+                         ),
+        ]
+
+    def setUp(self):
+        self.client = self.useFixture(mockpatch.Patch(
+            'aodh.evaluator.gnocchi.client'
+        )).mock.Client.return_value
+        super(TestEvaluate, self).setUp()
+
+    @staticmethod
+    def _get_stats(attr, value, count=1):
+        return statistics.Statistics(None, {attr: value, 'count': count})
+
+    @staticmethod
+    def _get_gnocchi_stats(granularity, values):
+        now = timeutils.utcnow_ts()
+        return [[six.text_type(now - len(values) * granularity),
+                 granularity, value] for value in values]
+
+    @staticmethod
+    def _reason(new_state, user_expression, causative_rules=(),
+                transition=True):
+        root_cause_rules = {}
+        for index, rule in causative_rules:
+            name = 'rule%s' % index
+            root_cause_rules.update({name: rule})
+        description = {evaluator.ALARM: 'outside their threshold.',
+                       evaluator.OK: 'inside their threshold.',
+                       evaluator.UNKNOWN: 'state evaluated to unknown.'}
+        params = {'state': new_state,
+                  'expression': user_expression,
+                  'rules': ', '.join(sorted(six.iterkeys(root_cause_rules))),
+                  'description': description[new_state]}
+        reason_data = {
+            'type': 'composite',
+            'composition_form': user_expression}
+        reason_data.update(causative_rules=root_cause_rules)
+        if transition:
+            reason = ('Composite rule alarm with composition form: '
+                      '%(expression)s transition to %(state)s, due to '
+                      'rules: %(rules)s %(description)s' % params)
+        else:
+            reason = ('Composite rule alarm with composition form: '
+                      '%(expression)s remaining as %(state)s, due to '
+                      'rules: %(rules)s %(description)s' % params)
+        return reason, reason_data
+
+    def test_simple_insufficient(self):
+        self._set_all_alarms('ok')
+        self.api_client.statistics.list.return_value = []
+        self.client.metric.aggregation.return_value = []
+        self.client.metric.get_measures.return_value = []
+        self._evaluate_all_alarms()
+        self._assert_all_alarms('insufficient data')
+        expected = [mock.call(alarm) for alarm in self.alarms]
+        update_calls = self.storage_conn.update_alarm.call_args_list
+        self.assertEqual(expected, update_calls)
+        expected = [mock.call(self.alarms[0],
+                              'ok',
+                              *self._reason(
+                                  'insufficient data',
+                                  '(rule1 or (rule2 and rule3))',
+                                  ((1, self.sub_rule1), (2, self.sub_rule2),
+                                   (3, self.sub_rule3)))),
+                    mock.call(self.alarms[1],
+                              'ok',
+                              *self._reason(
+                                  'insufficient data',
+                                  '(rule1 or rule2 or rule3)',
+                                  ((1, self.sub_rule1), (2, self.sub_rule2),
+                                   (3, self.sub_rule3)))),
+                    mock.call(self.alarms[2],
+                              'ok',
+                              *self._reason(
+                                  'insufficient data',
+                                  '(rule1 and rule2 and rule3)',
+                                  ((1, self.sub_rule1), (2, self.sub_rule2),
+                                   (3, self.sub_rule3)))),
+                    mock.call(
+                        self.alarms[3],
+                        'ok',
+                        *self._reason(
+                            'insufficient data',
+                            '(rule1 and rule2 and (rule3 or rule4 or rule5 '
+                            'or rule6))',
+                            ((1, self.sub_rule2), (2, self.sub_rule3),
+                             (3, self.sub_rule1), (4, self.sub_rule4),
+                             (5, self.sub_rule5), (6, self.sub_rule6))))]
+        self.assertEqual(expected, self.notifier.notify.call_args_list)
+
+    def test_alarm_full_trip_with_multi_type_rules(self):
+        alarm = self.alarms[3]
+        alarm.state = 'ok'
+        # following results of sub-rules evaluation to trigger
+        # final "alarm" state:
+        # self.sub_rule2: alarm
+        # self.sub_rule3: alarm
+        # self.sub_rule1: ok
+        # self.sub_rule4: ok
+        # self.sub_rule5: ok
+        # self.sub_rule6: alarm
+        maxs = [self._get_stats('max', self.sub_rule2['threshold'] + 0.01 * v)
+                for v in moves.xrange(1, 5)]
+        avgs1 = [self._get_stats('avg', self.sub_rule3['threshold'] + 0.01 * v)
+                 for v in moves.xrange(1, 4)]
+        avgs2 = [self._get_stats('avg', self.sub_rule1['threshold'] - 0.01 * v)
+                 for v in moves.xrange(1, 6)]
+
+        gavgs1 = self._get_gnocchi_stats(60, [self.sub_rule4['threshold']
+                                              - v for v in moves.xrange(1, 6)])
+        gmaxs = self._get_gnocchi_stats(300, [self.sub_rule5['threshold'] + v
+                                              for v in moves.xrange(1, 5)])
+        gavgs2 = self._get_gnocchi_stats(50, [self.sub_rule6['threshold'] + v
+                                              for v in moves.xrange(1, 7)])
+
+        self.api_client.statistics.list.side_effect = [maxs, avgs1, avgs2]
+        self.client.metric.get_measures.side_effect = [gavgs1]
+        self.client.metric.aggregation.side_effect = [gmaxs, gavgs2]
+        self.evaluator.evaluate(alarm)
+        self.assertEqual(3, self.api_client.statistics.list.call_count)
+        self.assertEqual(1, self.client.metric.get_measures.call_count)
+        self.assertEqual(2, self.client.metric.aggregation.call_count)
+        self.assertEqual('alarm', alarm.state)
+        expected = mock.call(
+            alarm, 'ok',
+            *self._reason(
+                'alarm',
+                '(rule1 and rule2 and (rule3 or rule4 or rule5 or rule6))',
+                ((1, self.sub_rule2), (2, self.sub_rule3),
+                 (6, self.sub_rule6))))
+        self.assertEqual(expected, self.notifier.notify.call_args)
+
+    def test_alarm_with_short_circuit_logic(self):
+        alarm = self.alarms[1]
+        # self.sub_rule1: alarm
+        avgs = [self._get_stats('avg', self.sub_rule1['threshold'] + 0.01 * v)
+                for v in moves.xrange(1, 6)]
+        self.api_client.statistics.list.side_effect = [avgs]
+        self.evaluator.evaluate(alarm)
+        self.assertEqual('alarm', alarm.state)
+        self.assertEqual(1, self.api_client.statistics.list.call_count)
+        expected = mock.call(self.alarms[1], 'insufficient data',
+                             *self._reason(
+                                 'alarm',
+                                 '(rule1 or rule2 or rule3)',
+                                 ((1, self.sub_rule1),)))
+        self.assertEqual(expected, self.notifier.notify.call_args)
+
+    def test_ok_with_short_circuit_logic(self):
+        alarm = self.alarms[2]
+        # self.sub_rule1: ok
+        avgs = [self._get_stats('avg', self.sub_rule1['threshold'] - 0.01 * v)
+                for v in moves.xrange(1, 6)]
+        self.api_client.statistics.list.side_effect = [avgs]
+        self.evaluator.evaluate(alarm)
+        self.assertEqual('ok', alarm.state)
+        self.assertEqual(1, self.api_client.statistics.list.call_count)
+        expected = mock.call(self.alarms[2], 'insufficient data',
+                             *self._reason(
+                                 'ok',
+                                 '(rule1 and rule2 and rule3)',
+                                 ((1, self.sub_rule1),)))
+        self.assertEqual(expected, self.notifier.notify.call_args)
+
+    def test_unknown_state_with_sub_rules_trending_state(self):
+        alarm = self.alarms[0]
+        maxs = [self._get_stats('max', self.sub_rule2['threshold'] + 0.01 * v)
+                for v in moves.xrange(-1, 4)]
+        avgs = [self._get_stats('avg', self.sub_rule3['threshold'] + 0.01 * v)
+                for v in moves.xrange(-1, 3)]
+        avgs2 = [self._get_stats('avg', self.sub_rule1['threshold'] - 0.01 * v)
+                 for v in moves.xrange(1, 6)]
+        self.api_client.statistics.list.side_effect = [avgs2, maxs, avgs]
+        self.evaluator.evaluate(alarm)
+        self.assertEqual('alarm', alarm.state)
+        expected = mock.call(self.alarms[0], 'insufficient data',
+                             *self._reason(
+                                 'alarm',
+                                 '(rule1 or (rule2 and rule3))',
+                                 ((2, self.sub_rule2),
+                                  (3, self.sub_rule3))))
+        self.assertEqual(expected, self.notifier.notify.call_args)
+
+    def test_known_state_with_sub_rules_trending_state(self):
+        alarm = self.alarms[0]
+        alarm.repeat_actions = True
+        alarm.state = 'ok'
+        maxs = [self._get_stats('max', self.sub_rule2['threshold'] + 0.01 * v)
+                for v in moves.xrange(-1, 4)]
+        avgs = [self._get_stats('avg', self.sub_rule3['threshold'] + 0.01 * v)
+                for v in moves.xrange(-1, 3)]
+        avgs2 = [self._get_stats('avg', self.sub_rule1['threshold'] - 0.01 * v)
+                 for v in moves.xrange(1, 6)]
+        self.api_client.statistics.list.side_effect = [avgs2, maxs, avgs]
+        self.evaluator.evaluate(alarm)
+        self.assertEqual('ok', alarm.state)
+        expected = mock.call(self.alarms[0], 'ok',
+                             *self._reason(
+                                 'ok',
+                                 '(rule1 or (rule2 and rule3))',
+                                 ((1, self.sub_rule1),
+                                  (2, self.sub_rule2),
+                                  (3, self.sub_rule3)), False))
+        self.assertEqual(expected, self.notifier.notify.call_args)
