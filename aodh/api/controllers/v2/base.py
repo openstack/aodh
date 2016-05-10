3daed76df892adefbebd897fc58f75679e1ab1e5
liusheng <liusheng@huawei.com>
2016-05-10 10:44:44 +0800

    [Trivial] Remove an unused exception definition
    Change-Id: Ib747fdbf1a58f5e574a2a38bc52b8d5ff2215782

--- aodh/api/controllers/v2/base.py
+++ aodh/api/controllers/v2/base.py
@@ -43,14 +43,6 @@ class ClientSideError(wsme.exc.ClientSideError):
         super(ClientSideError, self).__init__(error, status_code)
 
 
-class EntityNotFound(ClientSideError):
-    def __init__(self, entity, id):
-        super(EntityNotFound, self).__init__(
-            _("%(entity)s %(id)s Not Found") % {'entity': entity,
-                                                'id': id},
-            status_code=404)
-
-
 class ProjectNotAuthorized(ClientSideError):
     def __init__(self, id, aspect='project'):
         params = dict(aspect=aspect, id=id)
