acec4d5228d2ca3e3180dc21fda6b89d7f215ede
Mehdi Abaakouk <sileht@sileht.net>
2017-06-04 22:45:00 +0200

    Remove oslo.msg deprecation
    Change-Id: I8742a475de9f9e5c44509b3701601242ce71062f

--- aodh/messaging.py
+++ aodh/messaging.py
@@ -32,7 +32,7 @@ def get_transport(conf, url=None, optional=False, cache=True):
     transport = TRANSPORTS.get(cache_key)
     if not transport or not cache:
         try:
-            transport = oslo_messaging.get_transport(conf, url)
+            transport = oslo_messaging.get_notification_transport(conf, url)
         except (oslo_messaging.InvalidTransportURL,
                 oslo_messaging.DriverLoadFailure):
             if not optional or url:
