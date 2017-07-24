4cd17e6428a685813ef26e3d5fefa2f054d91f59
gord chung <gord@live.ca>
2017-07-24 22:16:10 +0000

    remove custom theming
    Change-Id: I72aedf072392d67d5a10946798dab12fab5abf66

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -118,13 +118,6 @@ nitpicky = False
 # html_theme_path = ['.']
 html_theme = 'openstackdocs'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    "nosidebar": "false"
-}
-
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = [openstackdocstheme.get_html_theme_path()]
 
